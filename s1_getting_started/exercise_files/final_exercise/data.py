393061252cdae5974f9ca29caf37f54e0d9dafcf
alaym3 <aam5xa@virginia.edu>
2023-01-02 21:34:41 +0100

    +ex

--- s1_getting_started/exercise_files/final_exercise/data.py
+++ s1_getting_started/exercise_files/final_exercise/data.py
@@ -1,8 +1,28 @@
 import torch
+import numpy as np
+import os
+import glob
 
 
 def mnist():
-    # exchange with the corrupted mnist dataset
-    train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784) 
-    return train, test
+   # initialize data arrays
+    X_train = []
+    y_train = []
+    X_test = []
+    y_test = []
+
+    # search all files inside a specific folder
+    # *.npz means file name only with npz extension
+    dir_path = r'/Users/alaina/Desktop/classes/2023Jan/dtu_mlops/data/corruptmnist/*.npz'
+    for file in glob.glob(dir_path, recursive=True):
+        # save the data and labels for each of the train and test files
+        # save training first
+        if 'train' in file:
+            X_train.append(np.load(str(file))['images'])
+            y_train.append(np.load(str(file))['labels'])
+        # save test second
+        elif 'test' in file:
+            X_test.append(np.load(str(file))['images'])
+            y_test.append(np.load(str(file))['labels'])
+    # return train, test
+    return (X_train, y_train) , (X_test, y_test)
--- s1_getting_started/exercise_files/final_exercise/model.py
+++ s1_getting_started/exercise_files/final_exercise/model.py
@@ -1,6 +1,22 @@
 from torch import nn
-
+import torch.nn.functional as F
 
 class MyAwesomeModel(nn.Module):
     def __init__(self):
         super().__init__()
+        # Inputs to hidden layer linear transformation
+        self.fc1 = nn.Linear(784, 128)
+        # Inputs to 2nd hidden layer linear transformation
+        self.fc2 = nn.Linear(128, 64)
+        # Output layer, 10 units - one for each digit
+        self.output = nn.Linear(64, 10)
+        
+    def forward(self, x):
+        # Hidden layer 1 with relu activation
+        x = F.relu(self.fc1(x))
+        # Hidden layer 2 with relu activation
+        x = F.relu(self.fc2(x))
+        # Output layer with softmax activation
+        x = F.log_softmax(self.output(x), dim=1)
+        
+        return x
